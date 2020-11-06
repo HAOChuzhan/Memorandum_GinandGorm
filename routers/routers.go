963a9a27132e99eece486b336125cb62c75f0722
Chuzhan Hao <haochuzhan@163.com>
2020-11-06 16:13:32 +0800

    Third

--- routers/routers.go
+++ routers/routers.go
@@ -3,6 +3,7 @@ package routers
 import (
 	"bubble/controller"
 	"bubble/setting"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -14,20 +15,20 @@ func SetupRouter() *gin.Engine {
 	// 告诉gin框架模板文件引用的静态文件去哪里找
 	r.Static("/static", "static")
 	// 告诉gin框架去哪里找模板文件
-	r.LoadHTMLGlob("templates/*")
+	//github.com/War11/Memorandum_GinandGorm.git
+	r.LoadHTMLGlob("/go/src/github.com/War11/Memorandum_GinandGorm/templates/*")
 	r.GET("/", controller.IndexHandler)
 
 	// v1
 	v1Group := r.Group("v1")
 	{
-		// 待办事项
-		// 添加
+
 		v1Group.POST("/todo", controller.CreateTodo)
-		// 查看所有的待办事项
+
 		v1Group.GET("/todo", controller.GetTodoList)
-		// 修改某一个待办事项
+
 		v1Group.PUT("/todo/:id", controller.UpdateATodo)
-		// 删除某一个待办事项
+
 		v1Group.DELETE("/todo/:id", controller.DeleteATodo)
 	}
 	return r
